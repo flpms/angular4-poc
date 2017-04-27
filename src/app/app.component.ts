63d4a98fae84dbfb549fe5b3853e27d670f3fef7
flpms <filipemelodasilva@gmail.com>
2017-04-27 19:56:23 -0300

    adjust component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Note } from './app.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  model: Note = new Note(1, '', '', '');
+
+  onSubmit(evt) {
+    console.log("= = = = 14", evt);
+    console.log(this.model);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
+import { PolymerModule } from '@codebakery/origami';
+import { IronElementsModule, PaperElementsModule } from '@codebakery/origami/lib/collections';
 
 import { AppComponent } from './app.component';
 
@@ -9,10 +12,18 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+  
+    FormsModule, // Required to connect elements to Angular forms
+    PolymerModule,
+
+    // Optional modules to help reduce markup complexity
+    IronElementsModule,
+    PaperElementsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
